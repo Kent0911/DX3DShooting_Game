e0724683e28e0a991248ccae3168d0d67f2b88b8
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-07-16 20:32:00 +0900

    WinMain function Created

---
+++ DX3DShooting_Game/GameSource/WinMain/WinMain.cpp
@@ -0,0 +1,20 @@
+#include < d3d11.h >
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow) {
+	
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	MSG msg = { 0 };
+	while ( WM_QUIT != msg.message ) {
+		if ( PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE )) {
+			TranslateMessage( &msg );
+			DispatchMessage( &msg );
+		}
+		else {
+
+		}
+	}
+
+	return static_cast< int >( msg.wParam );
+}
