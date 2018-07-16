a5c7703a1701be3b8076ce96c372e80288eba8e3
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-07-16 22:05:21 +0900

    Engine class added

--- DX3DShooting_Game/GameSource/WinMain/WinMain.cpp
+++ DX3DShooting_Game/GameSource/WinMain/WinMain.cpp
@@ -1,13 +1,19 @@
-#include < d3d11.h >
+#include <d3d11.h>
+
+#include "CKitEngine.h"
+
+extern kit::Engine::KitEngine g_application;
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow) {
 	
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
+	
+
 	MSG msg = { 0 };
 	while ( WM_QUIT != msg.message ) {
-		if ( PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE )) {
+		if ( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE )) {
 			TranslateMessage( &msg );
 			DispatchMessage( &msg );
 		}
