5faafa4d079e842a56f88fb4ff3e41d364e002a6
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-07-18 08:20:17 +0900

    Plane class added

--- DX3DShooting_Game/GameSource/GameObject/FlyingObject/Plane/Plane.cpp
+++ DX3DShooting_Game/GameSource/GameObject/FlyingObject/Plane/Plane.cpp
@@ -1,11 +1,16 @@
 #include "Plane.h"
 
-Plane::Plane() {
-	
+Plane::Plane( ID3D11Device* _pd3dDevice, bool _flag ) {
+	std::vector< kit::Engine::VertexPosNorTex > vertices = ChangeVertexType( *kit::LoadObjFile("Resources/Plane.obj") );
+	std::vector< uint16_t > indices = ChangeIndex( *kit::LoadObjFile("Resources/Plane.obj") );
+	m_model.Create( _pd3dDevice, vertices, indices );
+	WhoisControl( _flag );
+	if ( !mb_isComControl ) { mfunc_control = &Plane::Control; }
+	else if ( mb_isComControl ) { mfunc_control = &Plane::AutoControl; }
 }
 
 Plane::~Plane() {
-
+	m_model.Destroy();
 }
 
 void Plane::Control(kit::vec3 _target) {
@@ -17,7 +22,7 @@ void Plane::AutoControl(kit::vec3 _target) {
 }
 
 void Plane::Update(kit::vec3 _target) {
-
+	(this->*mfunc_control)(_target);
 }
 
 void Plane::Render(ID3D11DeviceContext* _pd3dImmidiateContext) {
--- DX3DShooting_Game/GameSource/GameObject/FlyingObject/Plane/Plane.h
+++ DX3DShooting_Game/GameSource/GameObject/FlyingObject/Plane/Plane.h
@@ -8,17 +8,26 @@
 #include "CVertexTypes.h"
 #include "../FlyingObject.h"
 
+enum class ControlIs {
+	Player = false,
+	CPU = true
+};
 
 class Plane : public FlyingObject {
 public:
-	Plane();
+	Plane( ID3D11Device* _pd3dDevice, bool _flag );
 	~Plane();
-	void(Plane::*mfunc_control)(kit::vec3);
-	void Control(kit::vec3 _target);
-	void AutoControl(kit::vec3 _target);
+	void( Plane::*mfunc_control )( kit::vec3 );
 
-	virtual void Update(kit::vec3 _target);
-	virtual void Render(ID3D11DeviceContext* _pd3dImmidiateContext);
+	inline void WhoisControl( bool _flag ) {
+		mb_isComControl = _flag;
+	}
+
+	virtual void Update( kit::vec3 _target );
+	virtual void Render( ID3D11DeviceContext* _pd3dImmidiateContext );
 private:
+	void Control( kit::vec3 _target );
+	void AutoControl( kit::vec3 _target );
 
+	bool mb_isComControl;
 };
